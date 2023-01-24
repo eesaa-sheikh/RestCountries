44e8e10d886e5f8b4207c6c7220567c25c62179f
eesaasheikh <eesaasheikh3@gmail.com>
2023-01-24 17:20:08 +0000

    fewChanges

--- src/components/Country.js
+++ src/components/Country.js
@@ -2,7 +2,9 @@
 const Country =({country}) =>{
     return (
         <div className="allCountry">
-        <h2 key = "uniqueId">{country.name.official},{country.flag},{country.population}</h2>
+        <h3 key = "uniqueId">{country.name.official},
+        {country.flag},
+        {country.population}</h3>
         <button className="visitedCountry">I Visited this Country</button>
         </div>
     )
