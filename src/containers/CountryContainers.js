1f88533a58da498d48f43af03f73f03eddd2c6a5
eesaasheikh <eesaasheikh3@gmail.com>
2023-01-24 20:09:32 +0000

    Testing

--- src/containers/CountryContainers.js
+++ src/containers/CountryContainers.js
@@ -26,74 +26,74 @@ const CountryContainers =() =>{
     }, [ ])
 
 
-    const[visitedTheCountries,setVisitedTheCountries] = useState([]);
+    // const[visitedTheCountries,setVisitedTheCountries] = useState([]);
 
-    const movetoVisited = (country) =>{
-        setVisitedTheCountries([...visitedTheCountries,country])
-    }
+    // const movetoVisited = (country) =>{
+    //     setVisitedTheCountries([...visitedTheCountries,country])
+    // }
 
 
-    // const visitingCountry = (Country) => {
+    const visitingCountry = (Country) => {
 
-    //     setVisitedTheCountries([...visitedTheCountries,Country]);
-    //     let filtered = country.filter((countries) =>country.name)
+        setVisitedTheCountries([...visitedTheCountries,Country]);
+        let filtered = country.filter((countries) =>country.name)
 
-    //   };
+      };
 
-// const countryComponents = countries.map((country)=>{
-//         return <Country country ={country} movetoVisited={movetoVisited}/>
-//     })
-//     const vistedCountryComp = visitedTheCountries.map((country) =>{
+const countryComponents = countries.map((country)=>{
+        return <Country country ={country} movetoVisited={movetoVisited}/>
+    })
+    const vistedCountryComp = visitedTheCountries.map((country) =>{
         
-//     return(
-//         <>
-//         <Country country={countryComponents} />
+    return(
+        <>
+        <Country country={countryComponents} />
         
         
         
-//         </>
-//     )
+        </>
+    )
 
-//     })
+    })
 
-//     return(
-//        countries.length > 0 ? countryComponents : <p>Loading Country</p>
+    return(
+       countries.length > 0 ? countryComponents : <p>Loading Country</p>
 
-//     )
-
-
-// };
+    )
 
 
-return(
-
-    <>
-        {
-        countries ? countries.map(function(country) { 
-            return (                
-            <>
-            <Country country={country} movetoVisited={movetoVisited}/>
-            </>)})
-            : "No countries found"
-        }
+};
 
 
-        <h2>Visited Countries:</h2>
-        {
-        visitedTheCountries ? visitedTheCountries.map(function(country) { 
-            return (                
-            <>
-            <Country country={country} visitedCountries={() => {console.log("Already added country!")}} />
-            </>)})
-            : "No countries found"
-        }
-    </>
+// return(
+
+//     <>
+//         {
+//         countries ? countries.map(function(country) { 
+//             return (                
+//             <>
+//             <Country country={country} movetoVisited={movetoVisited}/>
+//             </>)})
+//             : "No countries found"
+//         }
+
+
+//         <h2>Visited Countries:</h2>
+//         {
+//         visitedTheCountries ? visitedTheCountries.map(function(country) { 
+//             return (                
+//             <>
+//             <Country country={country} visitedCountries={() => {console.log("Already added country!")}} />
+//             </>)})
+//             : "No countries found"
+//         }
+//     </>
 
 
 
-)
+// )
 
-}
+// }
 
 
 
@@ -104,74 +104,3 @@ return(
 
 export default CountryContainers;
 
-
-// if button is clicked
-// countriy is moved from the arraylist
-// to the new array
-// header should say visited countries 
-
-
-
-
-
-
-
-
-// import { useState, useEffect } from "react";
-// import CountryComponent from "../components/CountryComponent";
-
-// const CountryContainer = () => {
-
-//     const[countries, setCountries] = useState([]);
-
-//     useEffect(() => {
-
-//         fetch("https://restcountries.com/v3.1/all")
-//         .then(response => response.json())
-//         .then(data => setCountries(data))
-
-//     }, [])
-
-//     const [visitedCountries, setVisitedCountries] = useState([]);
-
-//     const moveCountryToVisitedList = (country) => {
-//         setVisitedCountries([...visitedCountries, country])
-//     }
-    
-
-
-//     // const countryNames = countries.map((country) => country.name.common);
-
-//     // console.log(countryNames);
-
-//     console.log(visitedCountries)
-
-//     return(
-
-//         <>
-//             {
-//             countries ? countries.map(function(country) { 
-//                 return (                
-//                 <>
-//                 <CountryComponent country={country} moveCountryToVisitedList={moveCountryToVisitedList}/>
-//                 </>)})
-//                 : "No countries found"
-//             }
-    
-
-//             <h2>Visited Countries:</h2>
-//             {
-//             visitedCountries ? visitedCountries.map(function(country) { 
-//                 return (                
-//                 <>
-//                 <CountryComponent country={country} moveCountryToVisitedList={() => {console.log("Already added country!")}} />
-//                 </>)})
-//                 : "No countries found"
-//             }
-//         </>
-
-
-
-//     )
-
-// }
